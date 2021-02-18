3055e84c9bc822a6a70375a2cef6c03aab2b9fa5
Gabriel Pereira <78962152+GabriellPereiraa@users.noreply.github.com>
2021-02-18 15:09:04 -0300

    Update Topico03

--- Topico03.php
+++ Topico03.php
@@ -28,20 +28,120 @@ include('layout/header.php');
 
             <p class="Texto"><strong>Objetivo:</strong> Apresentar alguns sistemas éticos e seus principais teóricos</p>
             <hr class="hrLaranja">
+            <div class="container">
+                <p class="Texto">
+                    Ao longo da história, foram formulados diversos sistemas éticos. Os primeiros filósofos da Grécia Antiga se colocavam a pergunta, antes de tudo, pelo ser de todas as coisas, ou seja, pela sua “verdadeira realidade” ou sua “autêntica natureza”, uma vez que muitas vezes as “aparências enganam”. De modo que alguns deles não demoraram a impor-se a pergunta:
+                </p>
 
-            <p class="Texto">
-            Ao longo da história, foram formulados diversos sistemas éticos. Os primeiros filósofos da Grécia Antiga se colocavam a pergunta, antes de tudo, pelo ser de todas as coisas, ou seja, pela sua “verdadeira realidade” ou sua “autêntica natureza”, uma vez que muitas vezes as “aparências enganam”. De modo que alguns deles não demoraram a impor-se a pergunta:
+                <p class="Texto">
+                <i class="far fa-question-circle"></i> Qual a diferença entre ética e moral? Não seriam estas a mesma coisa?</h3>
+                </p>
+                
+                <br>
+                <div class="text-center">
+                    <p class="TituloFigura"><strong>Imagem 01:</strong> Estátua do filósofo grego, Sócrates </p>
+                    <a href="imagens/Imagem01.jpg" data-toggle="lightbox" data-footer="Estátua do filósofo grego, Sócrates">
+                        <img class="img-fluid" src="imagens/Imagem01.jpg" alt="Alt_da_Imagem">
+                    </a>
+                    <p class="FonteFigura"><strong>Fonte:</strong> Google</p>
+                </div>
 
-            </p>
+                <p class="Texto">
+                    Ainda que esse filósofo não tenha deixado nada escrito, os testemunhos de vários escritores da época nos permitem reconstruir algumas das principais contribuições que a ele são atribuídas. Uma delas é a ideia de que a excelência humana é caracterizada, antes de tudo, como uma atitude de busca constante pelo verdadeiro bem. Isso porque somente quem chega a conhecer tal bem é quem pode colocá-lo em prática. Assim, o primeiro passo para se alcançar a perfeiçãomoral é o abandono de atitudes dogmáticas e céticas e a consequente adoção de uma atitude crítica quesomente se deixa convencer pelo melhor argumento [racional].
+                </p>
+
+                <p class="Texto">
+                    É por nós conhecida a famosa frase “conhece-te a ti mesmo”, adotada por Sócrates, em sua juventude, ao lêla na fachada no oráculo de Delfos. Podemos dizer, ainda, que, para Sócrates, o objetivo último da busca da verdade não está na mera satisfação da curiosidade, mas na assimilação de conhecimentos para poder agir bem e para o bem, alcançando, desse modo, a excelência humana. Por esse motivo, sua ética é considerada como uma ética racionalista.
+                </p>
+                
+                <br>
+                <div class="text-center">
+                    <p class="TituloFigura"><strong>Imagem 01:</strong> Platão</p>
+                    <a href="imagens/Imagem02.jpg" data-toggle="lightbox" data-footer="Platão">
+                        <img class="img-fluid" src="imagens/Imagem02.jpg" alt="Alt_da_Imagem">
+                    </a>
+                    <p class="FonteFigura"><strong>Fonte:</strong> Google</p>
+                </div>
+
+                <p class="Texto">
+                        Discípulo e herdeiro do pensamento socrático, Platão sustenta que a verdadeira moral é um conhecimento que deve conduzir, ao mesmo tempo, a vida do indivíduo e a vida da comunidade, ou seja, a vida do cidadão e da pólis (“cidade” em grego). 
+                </p>
+
+                <p class="Texto">
+                        Ele propõe um modelo ideal, uma utopia, um desenho perfeito elaborado pela razão, destinado a servir de ponto de referência para todos aqueles que pretendam reformar os costumes e as instituições com o objetivo de melhorar a vida humana individual e social. Tal descrição está exposta em sua obra intitulada A República, na qual pretende demonstrar sua visão de justiça. Para ele, a virtude da justiça se apresenta como virtude que acolhe e harmoniza todo o conjunto da cidade e todos os elementos que conformam o indivíduo. Platão afirma que aqueles que se encontrarem de cheio com o bem em si, adquirirão um tipo de conhecimento superior, uma ciência do bem, que os capacitará de tal forma a governar com retidão e justiça nos assuntos da pólis-cidade. Talvez uma das frases mais marcantes da obra platônica aqui citada seja esta:
+                </p>
+
+                <p class="Texto">
+                    “Ora, a maior punição no caso de alguém que não deseja governar é ser governado por alguém pior do que ele próprio.”
+                </p>
+                <p class="Texto" style="color:var(--Cor01);">
+                    – (PLATÃO, 2005, p. 63.)
+                </p>
+
+                <p class="Texto">
+                    Daí a importância defendida por Platão de que o verdadeiro cidadão é aquele que se interessa e cuida dos interesses da cidade, da coletividade, e não apenas de seus próprios interesses e necessidades. Como o indivíduo não consegue, por si mesmo, aproximar-se da perfeição, ele precisaria do Estado – ou Comunidade Política – para construir-se e desenvolver-se. Em outras palavras, “o homem é bom enquanto bom cidadão”.
+                </p>
+
+                <h4>Eudemonismo</h4>
+                <p class="Texto">
+                    Eudemonismo do grego antigo εὐδαιμονισμός. Trata-se de uma doutrina filosófica que assume a felicidade (eudaimonia, em grego) como fundamento e princípio para uma vida moral. Nesse sistema, a felicidade ocupa o lugar central e influencia diretamente todo agir humano. Esse sistema compreende a felicidade como uma “satisfação espiritual duradoura”, que seria uma espécie de estado permanente de calma e de contentamento. Como adiantamos, o filósofo grego Aristóteles sustentava essa tese e chegava a asseverar que o comportamento ideal estava assentado nas práticas que conduziam o indivíduo ao objetivo final, que seria esse alto nível de felicidade ou bem estar. A felicidade não seria, em Aristóteles, um estado subjetivo, mas um modo de vida, em que se exercem determinadas virtudes. 
+                </p>
+                <p class="Texto">
+                    Com base nessa sua concepção, ele considera que a moral do cidadão individual se realiza da melhor maneira, à medida que ele busca – enquanto realiza seus interesses – os interesses do Estado. Isso quer dizer que a moral da pólis, da cidade, e a moral individual não podem ser concebidas de modo separado, isto é, a moral individual não se dá à margem da defesa da promoção dos interesses do coletivo. A ética, nesse sentido, não pode estar desvinculada da política e o mais alto bem individual, a felicidade, só é possível em uma pólis cujas leis são justas
+                </p>
+
+                <h4>Hedonismo</h4>
+                <p class="Texto">
+                    Etimologicamente, esta palavra pode significar “sistema de prazer”. Nesse sistema, o prazer é o valor supremo ao qual todos os outros valores estão subordinados. Estipula que existe apenas um bem para o ser humano, que é o prazer e apenas um mal que é a dor. Portanto, de modo muito resumido, o hedonismo pauta a conduta humana, visando à satisfação do prazer e à fuga da dor. Esta seria, consequentemente, a noção de vida feliz para esse sistema ético. No entanto, não se trata de um prazer pelo prazer, como uma proposta defensora de um tipo de libertinagem. Na realidade, o hedonismo propõe que se evitem os excessos, sofrimentos e desgastes. Para o hedonismo, a virtude está na disposição da alma em amadurecer os desejos a ponto de saber escolher os que mais lhe conferirão o maior gozo, alegria, a fim de obter o máximo de prazer.
+                </p>
+
+                <h4>Estoicismo</h4>
+                <p class="Texto">
+                    A ética estoica traz em si o mesmo ponto de partida das demais escolas gregas clássicas: a busca da felicidade. Esta escola leva esse nome por ter se originado nos pórticos (stoá, em grego) de Atenas e tem por fundador Zenão de Cítio (333 a 263 a.C.). 
+                </p>
+                <p class="Texto">
+                    A ética estoica pode ser resumida na seguinte expressão: “suporta e abstém”. Para o estoicismo, o bem supremo consiste em agir conforme a virtude e esta, por sua vez, é racional, portanto, trata-se de buscar agir de acordo com a razão. A virtude mais alta e cara ao estoicismo é a chamada ataraxia, que quer dizer, a imperturbabilidade, a ausência de perturbação, tranquilidade. Esse estado de perfeita paz da alma se alcança, conforme propõe o estoicismo, mediante a libertação das paixões. Para os estoicos, essa libertação ocorre com a virtude da justiça (julgar corretamente) e da fortaleza (quando o amor à virtude ajuda a suportar os males e as adversidades). O bem supremo dos estoicos é viver segundo a natureza, ou seja, de acordo com a razão, com a consciência do próprio destino e da função no universo, não se deixando conduzir pelas paixões ou afetos interiores ou, mesmo, por coisas exteriores.
+                </p>
+
+                <h4>Utilitarismo</h4>
+                <p class="Texto">
+                    Esta doutrina defende que a felicidade (ou bem supremo) para a qual deve tender o ser humano deve estar baseada no sentido do “útil”. Em termos práticos, trata-se de uma espécie de maximização da felicidade, que busca obter o maior proveito de todos os atos. Por esse motivo, tal sistema tem sido compreendido como uma forma renovada do hedonismo clássico.
+                </p>
+                <p class="Texto">
+                    Desse modo, o utilitarismo pode ser sintetizado na sentença: “é bom tudo o que é útil”, pois a utilidade passa a ser o seu princípio moral, a fim de obter a maior felicidade possível. Assim entendida, a conduta humana se dirige para um sentido de aproveitamento imediato de todas as ações. Como asseverou, certa vez, um tratadista de Direito Penal, em seu livro Sobre os delitos e as penas:
+                </p>
+
+                <p class="Texto">
+                “a máxima felicidade possível para o maior número possível de pessoas. ”
+                </p>
+                <p class="Texto" style="color:var(--Cor01);">
+                    (BECCARIA, 1764, p.183) –
+                </p>
+
+                <p class="Texto">
+                Assim, uma vez que a ética utilitarista se preocupa com o maior proveito que se possa extrair das suas ações, podemos dizer que se trata, pois, de uma ética do “sentido prático”.
+                </p>
+                <p class="Texto">
+                Assim, até mesmo alguma ação que tenha um caráter espiritual, para o utilitarismo, traz em seu bojo um meio de se alcançar alguma outra finalidade mais útil e prática, como um conforto e tranquilidade pessoal. Ou um ato de filantropia e de abnegação que, no seu fim, busca ostentar uma boa figura pública e receber admiração. Nesse sentido, é uma doutrina consequencialista, pois sustenta que as ações têm valor em relação à bondade ou à maldade que trazem. Seria como se se fizesse um cálculo dos benefícios que uma ação acarretará e se estes serão maiores que os malefícios. Caso se confirme essa “aritmética dos prazeres” (Jeremy Bentham), a ação poderá ser tida como útil, boa e aceitável. O utilitarismo é, pois, a corrente filosófica mais ampla do pragmatismo. Seus principais representantes foram Jeremy Bentham (17428-1832) e John Stuart Mill (1806-1873). 
+                </p>
+            </div>
         </div>
 
         <div class="center">
                 <div class="btn-group" id="btn-group">
-                    <a href="Topico01.php" class="btn ">Página Anterior</a>
+                    <a href="Topico02.php" class="btn ">Página Anterior</a>
                     <a href="#" class="btn ">Topo da Página</a>
-                    <a href="Topico03.php" class="btn ">Proxima Página</a>
+                    <a href="Topico04.php" class="btn ">Proxima Página</a>
                 </div>
         </div>
+        
+        <!-- SCRIPT LIGHTBOX -->
+        <script>
+                    $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+                        event.preventDefault();
+                        $(this).ekkoLightbox();
+                    });                    
+                </script>
 
     </div> <!-- CONTEUDO FIM -->
 
