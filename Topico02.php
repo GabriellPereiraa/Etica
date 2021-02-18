9f5c3e4b6d03609f4b584020383f07315c9b3f81
Gabriel Pereira <78962152+GabriellPereiraa@users.noreply.github.com>
2021-02-18 14:22:09 -0300

    Update Topico02 + Update Ux

--- Topico02.php
+++ Topico02.php
@@ -71,13 +71,155 @@ include('layout/header.php');
                     Vejamos, a seguir, um breve quadro de conteúdos da ética e da moral:
                 </p>
 
+                <div class="row justify-content-center">
+                    <div class="col col-lg-8 center borderAzul" style="background-color:#286ce6;">
+                        <p class="Texto" style="color:white">
+                            QUADRO DE CONTEÚDOS
+                        </p>
+                    </div>
+                </div>
+
+                <div class="row justify-content-center">
+                    <div class="col-4 borderAzul center">
+                        <p class="Texto">
+                            ÉTICA
+                        </p>
+                    </div>
+                    <div class="col-4 borderAzul center">
+                        <p class="Texto">
+                            MORAL
+                        </p>
+                    </div>
+                </div>
+
+                <div class="row justify-content-center">
+                    <div class="col-4 borderAzul" >
+                        <p class="Texto">
+                            <i class="fas fa-bullseye Azul"></i> Pensamento filosófico;
+                        </p>
+                        <p class="Texto">
+                            <i class="fas fa-bullseye Azul"></i> Ciência;
+                        </p>
+                        <p class="Texto">
+                            <i class="fas fa-bullseye Azul"></i> É teórica, contemplativa, abstrata;
+                        </p>
+                        <p class="Texto">
+                            <i class="fas fa-bullseye Azul"></i> Busca a universalidade.
+                        </p>
+                    </div>
+                    <div class="col-4 borderAzul">
+                        <p class="Texto">
+                            <i class="fas fa-bullseye Azul"></i> Objeto de estudo do pensamento filosófico (deontologia ou teoria dos deveres);
+                        </p>
+                        <p class="Texto">
+                            <i class="fas fa-bullseye Azul"></i> Objeto de estudo da ciência ética;
+                        </p>
+                        <p class="Texto">
+                            <i class="fas fa-bullseye Azul"></i> Conjunto de normas, princípios e valores com os quais se regula a conduta humana.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="row justify-content-center">
+                    <div class="col-4 borderAzul">
+                        <p class="Texto">
+                            <i class="fas fa-bullseye Azul"></i> Busca descrições e explicações gerais para os problemas morais.
+                        </p>
+                    </div>
+                    <div class="col-4 borderAzul">
+                        <p class="Texto">
+                            <i class="fas fa-bullseye Azul"></i> É prática;
+                        </p>
+                        <p class="Texto">
+                            <i class="fas fa-bullseye Azul"></i> Executa-se;
+
+                        </p>
+                        <p class="Texto">
+                            <i class="fas fa-bullseye Azul"></i> Exerce-se.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="row justify-content-center">
+                    <div class="col-4 borderAzul">
+                        <p class="Texto">
+                            <i class="fas fa-bullseye Azul"></i> Reflexão teórica.
+                        </p>
+                    </div>
+                    <div class="col-4 borderAzul">
+                        <p class="Texto">
+                            <i class="fas fa-bullseye Azul"></i> Modifica a própria vida.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="row justify-content-center">
+                    <div class="col-4 borderAzul">
+                        <p class="Texto">
+                            <i class="fas fa-bullseye Azul"></i> Conjunto de teorias acerca das obrigações normativas baseadas na observação dos atos reais da conduta.
+                        </p>
+                    </div>
+                    <div class="col-4 borderAzul">
+                        <p class="Texto">
+                            <i class="fas fa-bullseye Azul"></i> Conjunto de deveres, normas e valores.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="row justify-content-center">
+                    <div class="col-4 borderAzul">
+                        <p class="Texto">
+                            <i class="fas fa-bullseye Azul"></i> Seu caráter prático ou normativo é derivado de seu objeto de estudo, ou seja, da moral.
+                        </p>
+                    </div>
+                    <div class="col-4 borderAzul">
+                        <p class="Texto">
+                            <i class="fas fa-bullseye Azul"></i> Possui caráter eminentemente normativo e prático.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="row justify-content-center">
+                    <div class="col col-lg-8 center borderAzul" style="background-color:#286ce6;">
+                        <p class="Texto gridAzul" style="color:white;">
+                            AMBAS REALIDADES SÃO CONCOMITANTES
+                        </p>
+                    </div>
+                </div>
+
+                <br>
+                <p class="Texto">
+                Qualquer tentativa de definição do que venha a ser ética, assim como qualquer outra ciência, é resultado de uma investigação do pensamento sobre um objeto específico. Por isso, a ética é uma ciência normativa da atividade humana ordenada ao bem. Mais que isso, ao bem comum. Ela é uma ciência prática e normativa. Isso quer dizer que se dedica ao estudo dos atos, porém, não no sentido daquilo que estes são, mas como deveriam ser. Por esse motivo, o estudo da ética não ficou restrito ao campo da filosofia, mas dialoga e se faz presente em tantas outras ciências e áreas do saber. Por exemplo, a metafísica, a psicologia, a sociologia, a teologia, a religião e o direito são algumas das ciências e campos do saber afins que se relacionam com a ética, pois cada um desses saberes, a seu modo, refere-se às leis que regem o comportamento humano.
+                </p>
+
+                <div class="row">
+                    <div class="col-2 center border" style="background-color:#286ce6;">
+                        <p class="Texto" style="color:white;">
+                            Exemplo
+                        </p>
+                        
+                    </div>
+                    <div class="col-sm center border">
+                        <p class="Texto">
+                            Assim, quando alguém comete um roubo ou assassinato, não foi apenas sua mão que praticou tal ato, mas a pessoa em sua integralidade. Isso significa dizer que tantos elementos estão ali presentes: sua vontade, desejo, pensamento, emoções, intencionalidade, motivação, influências, conhecimento e assim por diante.
+                        </p>
+                    </div>
+                </div>
+
+                <p class="Texto">
+                A conduta é uma expressão da pessoa humana em sua integralidade. Desse modo, quando se julga um ato, é preciso valorá-lo em sua profundidade expressa na pessoa. Ao mesmo tempo, a ética considera o sujeito enquanto indivíduo e enquanto membro de uma sociedade com outros indivíduos.
+                </p>
+
+                <p class="Texto">
+                Nesse sentido, as estruturas que caracterizam o homem em seu aspecto individual e social entram no jogo das relações sociais. A pessoa humana é, por um lado, um indivíduo, entendido no sentido pleno. Em outras palavras: não dividido, não diluído. Entretanto, é, também, uma realidade que se “auto-possui”, mas que está aberta ao outro, às demais pessoas, formando, assim, um laço de relações comunitárias com o “nós”. Por isso, a ética – ou as teorias éticas – compreendem que a pessoa é complementada, realizada, com a estrutura da comunidade em que está inserida. Em outras palavras, a relação humana se dá a partir de um “eu” que se abre ao “outro eu”, a um “tu”, que se transforma em um “nós”.
+                </p>
             </div>
 
             <div class="center">
                 <div class="btn-group" id="btn-group">
-                    <a href="Apresentacao.php" class="btn ">Página Anterior</a>
+                    <a href="Topico01.php" class="btn ">Página Anterior</a>
                     <a href="#" class="btn ">Topo da Página</a>
-                    <a href="Topico02.php" class="btn ">Proxima Página</a>
+                    <a href="Topico03.php" class="btn ">Proxima Página</a>
                 </div>
             </div>
 
