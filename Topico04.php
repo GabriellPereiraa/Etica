1d23b9c4978fc88c6748fe754b55438aef25bf6d
Gabriel Pereira <78962152+GabriellPereiraa@users.noreply.github.com>
2021-02-19 10:54:05 -0300

    Update Topico4 Accordion

--- Topico04.php
+++ Topico04.php
@@ -33,89 +33,142 @@ include('layout/header.php');
             </p>
 
             <hr>
-            <h4>Max Scheler (1874-1928)</h4>
-            <p class="Texto">
-                Um dos principais axiólogos do século XX. Sua principal obra se intitula Ética. Nela, Scheler descreve que o dever não é o que fundamenta o valor, mas, ao contrário, que o valor é que fundamenta o dever. Ou seja, ele defende que existe uma materialidade e objetividade nos valores, o que vai na contrariedade do formalismo kantiano. Segundo Scheler, os valores são conhecidos mediante a intuição, não sendo acessíveis à razão. Desse modo, temos que os valores, para Scheler: são qualidades ideais, isto é, não são reais; são objetivos, ou seja, dão-se independentemente de serem conhecidos; são transcendentes e materiais.
-            </p>
-            <br>
+            <section id="Max">
+            <section id="Eudemonismo">
+                <div class="accordion" id="accordionExample">
+                    <div class="card">
+                        <div class="card-header" id="headingOne">
+                            <h2 class="mb-0">
+                                <button class="btn btn-success btn-block text-left collapsed " type="button" data-toggle="collapse" data-target="#collapse1" aria-expanded="false" aria-controls="collapse1">
+                                    <h3>Max Scheler (1874-1928)</h3>
+                                </button>
+                            </h2>
+                        </div>
+                        <div id="collapse1" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                            <div class="card-body">
+                                <p class="Texto">
+                                    Eudemonismo do grego antigo εὐδαιμονισμός. Trata-se de uma doutrina filosófica que assume a felicidade (eudaimonia, em grego) como fundamento e princípio para uma vida moral. Nesse sistema, a felicidade ocupa o lugar central e influencia diretamente todo agir humano. Esse sistema compreende a felicidade como uma “satisfação espiritual duradoura”, que seria uma espécie de estado permanente de calma e de contentamento. Como adiantamos, o filósofo grego Aristóteles sustentava essa tese e chegava a asseverar que o comportamento ideal estava assentado nas práticas que conduziam o indivíduo ao objetivo final, que seria esse alto nível de felicidade ou bem estar. A felicidade não seria, em Aristóteles, um estado subjetivo, mas um modo de vida, em que se exercem determinadas virtudes. 
+                                </p>
+                                <p class="Texto">
+                                <p class="Texto">
+                                    Um dos principais axiólogos do século XX. Sua principal obra se intitula Ética. Nela, Scheler descreve que o dever não é o que fundamenta o valor, mas, ao contrário, que o valor é que fundamenta o dever. Ou seja, ele defende que existe uma materialidade e objetividade nos valores, o que vai na contrariedade do formalismo kantiano. Segundo Scheler, os valores são conhecidos mediante a intuição, não sendo acessíveis à razão. Desse modo, temos que os valores, para Scheler: são qualidades ideais, isto é, não são reais; são objetivos, ou seja, dão-se independentemente de serem conhecidos; são transcendentes e materiais.
+                                </p>
+                                <br>
 
-            <div class="text-center">
-                <p class="TituloFigura"><strong>Imagem 03:</strong> Max Scheler </p>
-                <a href="imagens/Imagem03.jpg" data-toggle="lightbox" data-footer="Max Scheler">
-                    <img class="img-fluid" src="imagens/Imagem03.jpg" alt="Alt_da_Imagem">
-                </a>
-                <p class="FonteFigura"><strong>Fonte:</strong> Google</p>
-            </div>
-            
-            <hr>
-            <h4>Thomas Nagel (1937-)</h4>
-            <div class="text-center">
-                <p class="TituloFigura"><strong>Imagem 04:</strong> Thomas Nagel </p>
-                <a href="imagens/Imagem04.jpg" data-toggle="lightbox" data-footer="Thomas Nagel">
-                    <img class="img-fluid" src="imagens/Imagem04.jpg" alt="Alt_da_Imagem">
-                </a>
-                <p class="FonteFigura"><strong>Fonte:</strong> Google</p>
-            </div>
-
-            <p class="Texto">
-            Esse filósofo americano, à diferença das teses precedentes, não busca justificar a ação moral, mas procura explicar porque justificações tipicamente morais são, segundo seus termos, “capazes de nos seduzir”. O cético moral nega-se a ser persuadido por argumentos ou razões morais. Para ele, não basta produzir a crença de que certas proposições são verdadeiras. Uma possível estratégia seja demonstrar que
-            </p>
-
-            <div class="col-md-12">
-                <blockquote class="blockquoteEad">
-                    <p class="mb-0">“tal ceticismo está em radical conflito com as intuições práticas às quais cada um está sujeito e que são suscetíveis de sistematização.”</p>
-                    <footer class="blockquote-footer">– (NAGEL, 1970, p. 144.)</footer>
-                </blockquote>
-            </div>
-            <br>
-
-            <p class="Texto">
-            Esse autor sustenta que, assim como existem as exigências racionais para o pensamento, também existem as exigências para a ação, sendo o altruísmo uma delas. Nagel entende altruísmo como 
-            </p>
-            
-            <div class="col-md-12">
-                <blockquote class="blockquoteEad">
-                    <p class="mb-0">“a vontade de agir em consideração do interesse de outras pessoas, sem necessidade de outros motivos ulteriores. ”</p>
-                    <footer class="blockquote-footer">(NAGEL, 1970, p. 79.) -</footer>
-                </blockquote>
-            </div>
-            <br>
-
-            <p class="Texto">
-                Posto que, para Nagel, o altruísmo é uma condição da racionalidade, ele estaria subjacente à ética, não como meramente um sentimento, senão que dependente do reconhecimento dos outros e dependente de ver a si mesmo como um simples indivíduo entre muitos.
-            </p>
-
-            <p class="Texto">
-                De certa forma, Nagel se une à tese de Kant do seu imperativo categórico, no sentido em que procura descobrir exigências para a ação que se aplique ao ser humano, sem nenhuma condição sobre o que quer ou sente, ou seja, sem envolver sentimento ou vontade, mas, apenas, racionalidade. Finalmente, Thomas Nagel sustenta em favor do altruísmo argumentando que os interesses e a felicidade humana têm valor como fins e que tal valor deve ser concebido como algo objetivo, pois assim
-            </p>
-            
-            <div class="col-md-12">
-                <blockquote class="blockquoteEad">
-                    <p class="mb-0">“significará que os fins são objetos racionais de atenção comum a todos. ”</p>
-                    <footer class="blockquote-footer">(KANT, 2008, p. 296.) –</footer>
-                </blockquote>
-            </div>
-            <br>
+                                <div class="text-center">
+                                    <p class="TituloFigura"><strong>Imagem 03:</strong> Max Scheler </p>
+                                    <a href="imagens/Imagem03.jpg" data-toggle="lightbox" data-footer="Max Scheler">
+                                        <img class="img-fluid" src="imagens/Imagem03.jpg" alt="Alt_da_Imagem">
+                                    </a>
+                                    <p class="FonteFigura"><strong>Fonte:</strong> Google</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
 
             <hr>
-            <h4>Jürgen Habermas (1929-)</h4>
-            <div class="text-center">
-                <p class="TituloFigura"><strong>Imagem 05:</strong> Jürgen Habermas </p>
-                <a href="imagens/Imagem05.jpg" data-toggle="lightbox" data-footer="Jürgen Habermas">
-                    <img class="img-fluid" src="imagens/Imagem05.jpg" alt="Alt_da_Imagem">
-                </a>
-                <p class="FonteFigura"><strong>Fonte:</strong> Google</p>
-            </div>
 
-            <p class="Texto">
-                Por seu turno, o filósofo alemão Habermas pretende, também, uma justificação das normas morais, de ordem objetivas e universais. Entretanto, diferentemente de Kant, ele pensa que as argumentações morais devem sair do âmbito da subjetividade e da individualidade, para se instalarem no espaço intersubjetivo, a partir do qual possam abordar a tarefa da justificação da universalidade e da objetividade da moral. Segundo ele, não é suficiente elaborar um ponto de vista moral que assegure a imparcialidade e que as pessoas aceitem adotá-lo enquanto seres livres e iguais.
-            </p>
-            <p class="Texto">
-                As argumentações morais, conforme ele, somente são possíveis de serem resolvidas, através do diálogo, como resultado da cooperação. Habermas defende o pluralismo cultural, ou seja, existe uma multiplicidade de perspectivas interpretativas, sendo essa a razão pela qual o significado do princípio de universalização não fica suficientemente esgotado por nenhuma reflexão monológica (pensamento individual). Assim, para universalizar as normas de um modo adequado, não basta uma reflexão pessoal, em que tentamos imaginar um ponto de vista em que todos compartilhariam.
-            </p>
-            <p class="Texto">
-                Habermas ensaia fundamentar sua ética através da lógica da argumentação moral. Com isso, a fundamentação da ética só é possível se pudermos observar, previamente, que se produzem de fato argumentações morais – o que ele chama de mundo da vida –, isto é, na vida corrente, cotidiana. Dessa maneira, antes de iniciar a fundamentação, é preciso constatar que os fenômenos morais são produzidos na vida dos seres humanos, que em suas ações comunicativas manifestam pretensões de validade das normas morais. Resumindo, a ética habermasiana é concebida como uma explicação de um certo tipo de fenômeno: a validade das normas e das pretensões de validade que realizamos quando falamos sobre as normas. 
-            </p>   
+            <section id="Thomas">
+                <div class="accordion" id="accordionExample">
+                    <div class="card">
+                        <div class="card-header" id="headingOne">
+                            <h2 class="mb-0">
+                                <button class="btn btn-success btn-block text-left collapsed " type="button" data-toggle="collapse" data-target="#collapse2" aria-expanded="false" aria-controls="collapse2">
+                                    <h3>Thomas Nagel (1937-)</h3>
+                                </button>
+                            </h2>
+                        </div>
+                        <div id="collapse2" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                            <div class="card-body">
+                                <div class="text-center">
+                                    <p class="TituloFigura"><strong>Imagem 04:</strong> Thomas Nagel </p>
+                                    <a href="imagens/Imagem04.jpg" data-toggle="lightbox" data-footer="Thomas Nagel">
+                                        <img class="img-fluid" src="imagens/Imagem04.jpg" alt="Alt_da_Imagem">
+                                    </a>
+                                    <p class="FonteFigura"><strong>Fonte:</strong> Google</p>
+                                </div>
+
+                                <p class="Texto">
+                                Esse filósofo americano, à diferença das teses precedentes, não busca justificar a ação moral, mas procura explicar porque justificações tipicamente morais são, segundo seus termos, “capazes de nos seduzir”. O cético moral nega-se a ser persuadido por argumentos ou razões morais. Para ele, não basta produzir a crença de que certas proposições são verdadeiras. Uma possível estratégia seja demonstrar que
+                                </p>
+
+                                <div class="col-md-12">
+                                    <blockquote class="blockquoteEad">
+                                        <p class="mb-0">“tal ceticismo está em radical conflito com as intuições práticas às quais cada um está sujeito e que são suscetíveis de sistematização.”</p>
+                                        <footer class="blockquote-footer">– (NAGEL, 1970, p. 144.)</footer>
+                                    </blockquote>
+                                </div>
+                                <br>
+
+                                <p class="Texto">
+                                Esse autor sustenta que, assim como existem as exigências racionais para o pensamento, também existem as exigências para a ação, sendo o altruísmo uma delas. Nagel entende altruísmo como 
+                                </p>
+                                
+                                <div class="col-md-12">
+                                    <blockquote class="blockquoteEad">
+                                        <p class="mb-0">“a vontade de agir em consideração do interesse de outras pessoas, sem necessidade de outros motivos ulteriores. ”</p>
+                                        <footer class="blockquote-footer">(NAGEL, 1970, p. 79.) -</footer>
+                                    </blockquote>
+                                </div>
+                                <br>
+
+                                <p class="Texto">
+                                    Posto que, para Nagel, o altruísmo é uma condição da racionalidade, ele estaria subjacente à ética, não como meramente um sentimento, senão que dependente do reconhecimento dos outros e dependente de ver a si mesmo como um simples indivíduo entre muitos.
+                                </p>
+
+                                <p class="Texto">
+                                    De certa forma, Nagel se une à tese de Kant do seu imperativo categórico, no sentido em que procura descobrir exigências para a ação que se aplique ao ser humano, sem nenhuma condição sobre o que quer ou sente, ou seja, sem envolver sentimento ou vontade, mas, apenas, racionalidade. Finalmente, Thomas Nagel sustenta em favor do altruísmo argumentando que os interesses e a felicidade humana têm valor como fins e que tal valor deve ser concebido como algo objetivo, pois assim
+                                </p>
+                                
+                                <div class="col-md-12">
+                                    <blockquote class="blockquoteEad">
+                                        <p class="mb-0">“significará que os fins são objetos racionais de atenção comum a todos. ”</p>
+                                        <footer class="blockquote-footer">(KANT, 2008, p. 296.) –</footer>
+                                    </blockquote>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <hr>
+            
+            <section id="Jurgen">
+                <div class="accordion" id="accordionExample">
+                    <div class="card">
+                        <div class="card-header" id="headingOne">
+                            <h2 class="mb-0">
+                                <button class="btn btn-success btn-block text-left collapsed " type="button" data-toggle="collapse" data-target="#collapse3" aria-expanded="false" aria-controls="collapse3">
+                                    <h3>Jürgen Habermas (1929-)</h3>
+                                </button>
+                            </h2>
+                        </div>
+                        <div id="collapse3" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                            <div class="card-body">
+                                <div class="text-center">
+                                    <p class="TituloFigura"><strong>Imagem 05:</strong> Jürgen Habermas </p>
+                                    <a href="imagens/Imagem05.jpg" data-toggle="lightbox" data-footer="Jürgen Habermas">
+                                        <img class="img-fluid" src="imagens/Imagem05.jpg" alt="Alt_da_Imagem">
+                                    </a>
+                                    <p class="FonteFigura"><strong>Fonte:</strong> Google</p>
+                                </div>
+
+                                <p class="Texto">
+                                    Por seu turno, o filósofo alemão Habermas pretende, também, uma justificação das normas morais, de ordem objetivas e universais. Entretanto, diferentemente de Kant, ele pensa que as argumentações morais devem sair do âmbito da subjetividade e da individualidade, para se instalarem no espaço intersubjetivo, a partir do qual possam abordar a tarefa da justificação da universalidade e da objetividade da moral. Segundo ele, não é suficiente elaborar um ponto de vista moral que assegure a imparcialidade e que as pessoas aceitem adotá-lo enquanto seres livres e iguais.
+                                </p>
+                                <p class="Texto">
+                                    As argumentações morais, conforme ele, somente são possíveis de serem resolvidas, através do diálogo, como resultado da cooperação. Habermas defende o pluralismo cultural, ou seja, existe uma multiplicidade de perspectivas interpretativas, sendo essa a razão pela qual o significado do princípio de universalização não fica suficientemente esgotado por nenhuma reflexão monológica (pensamento individual). Assim, para universalizar as normas de um modo adequado, não basta uma reflexão pessoal, em que tentamos imaginar um ponto de vista em que todos compartilhariam.
+                                </p>
+                                <p class="Texto">
+                                    Habermas ensaia fundamentar sua ética através da lógica da argumentação moral. Com isso, a fundamentação da ética só é possível se pudermos observar, previamente, que se produzem de fato argumentações morais – o que ele chama de mundo da vida –, isto é, na vida corrente, cotidiana. Dessa maneira, antes de iniciar a fundamentação, é preciso constatar que os fenômenos morais são produzidos na vida dos seres humanos, que em suas ações comunicativas manifestam pretensões de validade das normas morais. Resumindo, a ética habermasiana é concebida como uma explicação de um certo tipo de fenômeno: a validade das normas e das pretensões de validade que realizamos quando falamos sobre as normas. 
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
 
         <br>
