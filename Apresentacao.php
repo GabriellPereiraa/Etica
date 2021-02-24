06ea64379c6499e3f623102ca4c64ab734b09f53
Gabriel Pereira <78962152+GabriellPereiraa@users.noreply.github.com>
2021-02-24 08:24:30 -0300

    Update Apresentacao.php

--- Apresentacao.php
+++ Apresentacao.php
@@ -19,22 +19,24 @@ include('layout/header.php');
         <!-- CONTEUDO INICIO -->
         <div class="main-content container-fluid">
             <div class="page-title">
-                <h2 class="font-bold">
+                <h2 class="font-bold" style="color:#008445  ">
                     Apresentação
                 </h2>
+                <hr>
                     <p class="Texto">
                         Desde tempos imemoráveis, a conduta humana tem sido objeto de estudo. A ética é um dos tantos ramos da filosofia e se dedica, resumidamente, ao estudo da moral em todas as suas manifestações. Ela é uma das mais altas ou mais avançadas maneiras de harmonização e convivência social. Nas décadas recentes, a abordagem sobre ética vem adquirindo cada vez mais espaço, em particular, em virtude da grande extensão de práticas perversas que tem se apresentado em grande partes do mundo, em particular, em diversas instituições públicas e privadas. Assim, algumas questões contemporâneas são postas pela ética, dentre elas: em que consiste a felicidade? O que torna uma ação boa ou má? Justa ou injusta?
                     </p>
 
                 <br>
                 <hr>
-                <h2 class="font-bold">
+                <h2 class="font-bold" style="color:#008445">
                     Objetivo
                 </h2>
                     <p class="Texto">
-                        <i class="fas fa-bullseye"></i> Apresentar uma sumária conceituação de ética e moral, bem como alguns sistemas éticos e seus teóricos ao longo da história (períodos clássico grego, moderno e contemporâneo).
+                        <i class="fas fa-bullseye CorLaranja"></i> Apresentar uma sumária conceituação de ética e moral, bem como alguns sistemas éticos e seus teóricos ao longo da história (períodos clássico grego, moderno e contemporâneo).
                     </p>
             </div> <!-- CLASS PAGE TITLE FIM -->
+            <br><br>
             
             <div class="center">
                 <div class="btn-group" id="btn-group">
